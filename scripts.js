58a4be1a4e881091f6697c69d5e72c8ad8a117c6
Brian <brian.hargrove@outlook.com>
2020-09-30 00:25:49 -0400

    0930 0025

--- scripts.js
+++ scripts.js
@@ -1,28 +1,25 @@
 let baseURL = 'https://ergast.com/api/f1/driverStandings/1.json';
 let limit = '?limit=100';
-const searchForm = document.querySelector('form');
-const submitBtn = document.querySelector('.submit');
-
+let select = document.querySelector('select');
 let winnerList = document.querySelector('.yearResults');
 
+//let selecetdropdown = document.addEventListener('change', displayResult);
 
-searchForm.addEventListener('submit',fetchResults);
 
-
-fetch(baseURL+limit)  
+fetch(baseURL+limit)
 .then(function(drivers){
-    //console.log(drivers);
     return drivers.json();
-}).then(function(json){
-    console.log('driver:',json);
+}).then(function(json) {
     displayOptions(json);
     fetchResults(json);
-   });
+    //displayResults(json);
+    console.log(json);
+});
 
-
-
+//Populates Dropdown begin
 function displayOptions(json){
     let year = json.MRData.StandingsTable.StandingsLists;
+    console.log(year);
     let dropdown = document.getElementById('dropdown');
     console.log('n:', year);
     
@@ -31,42 +28,45 @@ function displayOptions(json){
         option.innerHTML = year[i].season;
         option.value = year[i].season;
         dropdown.add(option);
-    }console.log('morgan',year);
-  return year;
+        console.log('year',year);
+    };
+     return year;
 };
+//Populate Dropdown end
 
-//? if year = info, then show familyName + givenName
-
-function fetchResults(json) {
-    console.log('bench',json);
+function fetchResults(json){
     let info = json.MRData.StandingsTable.StandingsLists;
-    console.log('rose',info);
+        console.log('davis:',info);
        
-        for (let j=0; j<info.length; j++) {
-            let standings = info[j].DriverStandings; 
-            console.log(standings);
+
+    for (let j=0; j<info.length; j++){
+        let standings = info[j].DriverStandings;
+        console.log(standings);
+       
+
+        for (let k=0; k<standings.length; k++){
+            //console.log('votto',standings[k].Driver.givenName);
+            let champion = standings[k].Driver;
+            console.log('bauer',champion);
+                      
             
-            for(let k=0; k<standings.length; k++){
-                console.log(standings[k].Driver.familyName);
+            let heading = document.createElement('ul');
+            
+            heading.innerHTML=`${standings[k].Driver.givenName} ${standings[k].Driver.familyName} <br> ${standings[k].Driver.nationality}`;
+            winnerList.appendChild(heading); 
+    }
+        }       
+
+                }
                
-                let heading = document.createElement('p');
-                heading.innerHTML=`${standings[k].Driver.givenName} ${standings[k].Driver.familyName}`;
-                winnerList.appendChild(heading);      
-                
-                
-                
-            }
+// function displayResult(year){
+//     if(option.value===year.season){
+//                     let heading = document.createElement('ul');
+//                     heading.innerHTML=`${standings[k].Driver.givenName} ${standings[k].Driver.familyName} <br> ${standings[k].Driver.nationality}`;
+//              winnerList.appendChild(heading); 
+//              }};
+        
 
-} 
+    
 
 
-}
-// function displayResults(json){
-//     let champion = standings;
-//     console.log('rijo',champion);
-//     if(year==info);{
-       
-//     };
-// }
-
-// //? 1952 = MRData.StandingsTable.StandingsLists[2].DriverStandings[1].Driver.familyName + givenName
